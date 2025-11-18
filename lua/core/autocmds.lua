734c3a8a200d84d17ab543a39a6633052beac4cd
Katchau Berg <69154117+KatchauBerg@users.noreply.github.com>
2025-11-18 09:11:56 -0300

    Update CHADVIM

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -3,11 +3,37 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
--- Adicione seus autocomandos aqui
--- Exemplo:
--- augroup('MyAutocmds', { clear = true })
--- autocmd('BufWritePre', {
---   group = 'MyAutocmds',
---   pattern = '*',
---   command = 'echo "Escrevendo no buffer..."'
--- })
+-- Grupo para autocomandos de carregamento e salvamento de tema
+augroup('ThemeHandler', { clear = true })
+
+-- Carrega o esquema de cores ao entrar no Neovim
+autocmd('VimEnter', {
+  group = 'ThemeHandler',
+  pattern = '*',
+  callback = function()
+    -- Carrega o tema salvo
+    local status, theme = pcall(require, 'colorscheme')
+    if status and theme then
+      vim.cmd.colorscheme(theme)
+    else
+      -- Fallback se o arquivo não existir ou estiver vazio
+      vim.cmd.colorscheme('catppuccin')
+    end
+  end,
+})
+
+-- Salva o esquema de cores sempre que for alterado
+autocmd('ColorScheme', {
+  group = 'ThemeHandler',
+  pattern = '*',
+  callback = function(args)
+    local theme = args.match
+    if theme then
+      local file = io.open(vim.fn.stdpath('config') .. '/colorscheme.lua', 'w')
+      if file then
+        file:write('return "' .. theme .. '"')
+        file:close()
+      end
+    end
+  end,
+})
--- lua/plugins/hackthebox.lua
+++ lua/plugins/hackthebox.lua
@@ -1,7 +1,3 @@
 return {
   "audibleblink/hackthebox.vim",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "hackthebox"
-  end,
 }
