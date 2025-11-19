6b564c87308816fc84488674f0cdcc7bb5a57cbf
dev2 inside lockers <dev2.insidelockers@gmail.com>
2025-11-19 17:33:55 -0300

    [UPDATE] - Refinando NVIM Config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,11 @@
 return {
   "akinsho/bufferline.nvim",
   -- cond = not vim.g.vscode,
+  keys = {
+    { "<Tab>", "<cmd> BufferLineCycleNext <CR>", desc = "BufferLine: Next Buffer" },
+    { "<S-Tab>", "<cmd> BufferLineCyclePrev <CR>", desc = "BufferLine: Previous Buffer" },
+    { "<C-q>", "<cmd> bd <CR>", desc = "BufferLine: Close Buffer" },
+  },
   opts = {
     options = {
       themable = true,
