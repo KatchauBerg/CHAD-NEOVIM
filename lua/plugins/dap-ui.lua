07191d10bb369d0ab024bf60e38c22783f9e3044
dev2 inside lockers <dev2.insidelockers@gmail.com>
2025-11-28 14:54:38 -0300

    [UPDATE] - Atualizando configs

--- lua/plugins/dap-ui.lua
+++ lua/plugins/dap-ui.lua
@@ -8,6 +8,8 @@ return {
     config = function()
         local dap = require("dap")
         local dapui = require("dapui")
+
+        -- 1. Configuração da UI (Visual)
         dapui.setup({
             icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
             controls = {
@@ -26,26 +28,33 @@ return {
             },
         })
 
-        dap.adapters.cppdbg = {
-            type = 'executable',
-            command = '/home/dev2/.local/share/nvim/mason/bin/codelldb', 
-            name = 'codelldb',
+        -- 2. Configuração do Adaptador (O Cérebro)
+        -- Mudamos para 'server' pois o codelldb funciona melhor assim
+        dap.adapters.codelldb = {
+            type = 'server',
+            port = "${port}",
+            executable = {
+                -- CAMINHO DINÂMICO: Usa o diretório padrão do Mason
+                command = vim.fn.stdpath("data") .. '/mason/bin/codelldb', 
+                args = {"--port", "${port}"},
+            }
         }
 
+        -- 3. Configuração de Lançamento (Como rodar o C++)
         dap.configurations.cpp = {
             {
                 name = "Launch C++ Executável",
-                type = "cppdbg", -- DEVE CORRESPONDER AO ADAPTER ID ACIMA
+                type = "codelldb", -- TEM QUE SER IGUAL AO NOME DO ADAPTER ACIMA
                 request = "launch",
                 program = function()
-                    -- Pergunta ao usuário o caminho do binário compilado
                     return vim.fn.input('Caminho para o executável: ', vim.fn.getcwd() .. '/', 'file')
                 end,
                 cwd = '${workspaceFolder}',
-                stopOnEntry = true,
+                stopOnEntry = false, -- Geralmente false é melhor para ir direto ao main ou breakpoint
             },
         }
 
+        -- 4. Listeners (Automação da UI)
         dap.listeners.after.event_initialized["dapui_config"] = function()
             dapui.open()
         end
@@ -56,21 +65,24 @@ return {
             dapui.close()
         end
 
+        -- 5. Keymaps (Seus atalhos)
         local function dap_launch()
-            dapui.open()
-            dap.run_last() 
+            -- Tenta rodar a ultima sessão, se não existir, começa uma nova
+            if dap.session() then
+                dap.continue()
+            else
+                dap.continue() 
+            end
         end
 
-        vim.keymap.set('n', '<Leader><F6>', dap_launch, { desc = 'DAP: Iniciar/Lançar Sessão' })
-        vim.keymap.set('n', '<F5>', dap.continue, { desc = 'DAP: Continuar/Parar' })
+        vim.keymap.set('n', '<F5>', dap.continue, { desc = 'DAP: Iniciar/Continuar' })
         vim.keymap.set('n', '<Leader>c', dap.terminate, { desc = 'DAP: Parar Sessão' })
         
-        vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'DAP: Step Over' })
-        vim.keymap.set('n', '<F11>', dap.step_into, { desc = 'DAP: Step Into' })
-        vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'DAP: Step Out' })
+        vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'DAP: Step Over (Pular linha)' })
+        vim.keymap.set('n', '<F11>', dap.step_into, { desc = 'DAP: Step Into (Entrar na função)' })
+        vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'DAP: Step Out (Sair da função)' })
 
-        vim.keymap.set('n', '<Leader>b', dap.toggle_breakpoint, { desc = 'DAP: Toggle Breakpoint' })
-        vim.keymap.set('n', '<Leader>du', dapui.toggle, { desc = 'DAP UI: Toggle Interface' })
-
+        vim.keymap.set('n', '<Leader>b', dap.toggle_breakpoint, { desc = 'DAP: Breakpoint' })
+        vim.keymap.set('n', '<Leader>du', dapui.toggle, { desc = 'DAP UI: Abrir/Fechar Manualmente' })
     end,
 }
